385be23a1a70868a0d28987061636088c9a96d03
sushiroll <jjames@lullipop.com>
2022-12-25 23:52:54 +0000

    init commit

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
+
+function MyApp({ Component, pageProps }) {
+ 
+  
+  return (
+    
+     
+        <Component {...pageProps}  />
+    
+    
+  )
+}
+
+export default MyApp
