df42373d4bc4dcd2be011900bae28f64d04cd1de
sushiroll <jjames@lullipop.com>
2022-12-26 00:39:00 +0000

    init

--- components/Nav.js
+++ components/Nav.js
@@ -4,7 +4,13 @@ export default function Nav() {
   return (
     <header className="border-b sticky top-0 bg-stone-600">
         <div className="flex items-center justify-between max-w-6xl pt-4 pb-2 px-4 mx-auto lg:max-w-screen-xl">
-            <Link href="/" passHref className='cu'
+            <Link href="/" passHref className='cursor-pointer'>
+              <span className='text-lg pt-1 font-blod'></span>
+              Shopify + Next.js
+            </Link>
+            <a className='text-md font-bold cursor-pointer'>
+              Cart
+            </a>
         </div>
     </header>
   )
